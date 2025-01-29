2c4d484c0c0bf01bb7205ee99787aec92b200478
Parth Gala <122494475+parth-galaaa@users.noreply.github.com>
2025-01-29 18:52:06 -0500

    prod test

--- components/AnalyzerTools.tsx
+++ components/AnalyzerTools.tsx
@@ -27,7 +27,7 @@ const TabsComponent = () => {
 	const handleAction = async (tab: keyof typeof inputs) => {
 		setEnabledTab(tab);
 		try {
-			const response = await fetch("http://127.0.0.1:5328/api/textanalyze", {
+			const response = await fetch("/api/textanalyze", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({
